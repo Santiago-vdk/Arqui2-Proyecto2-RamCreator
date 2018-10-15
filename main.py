d81503fe262b73734df5e3503542526fe57e07ff
Santiago VdeK <santiago.vdk@gmail.com>
2018-10-15 10:47:43 -0600

    Cambiar a recorrido de 4 pixeles

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ import numpy as np
 import cv2
 from bitstring import BitArray
 
-
 while(1):
     print("Digite 1 para crear MIF ")
-    print("Digite 2 para reconstruir imagen")
     print("Digite 'q' para cerrar la ejecucion")
-    opcion= input("Opcion: ")
+    opcion = input("Opcion: ")
     if(opcion == "q"):
-            break
+        break
     elif(int(opcion) == 1):
         img = input("Digite el nombre de su imagen: ")
         if(img == "q"):
@@ -18,20 +16,20 @@ while(1):
         print("Creando archivo MIF...")
         image = cv2.imread(img)
         image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        resize = cv2.resize(image_gray, (150,150)) 
+        resize = cv2.resize(image_gray, (100, 100))
 
-        cv2.imwrite('resize.jpg', resize)
-        cv2.imwrite('output.jpg', resize)
-        output = cv2.imread('output.jpg')
+        cv2.imwrite('Output/resize_' + img, resize)
+        cv2.imwrite('Output/output_' + img, resize)
+        output = cv2.imread('output_' + img)
 
-        f = open("RAM.mif","w+")
-        
-        f.write("WIDTH = 8;")
-        f.write("\n")        
-        f.write("DEPTH = 65536;")
+        f = open("Output/RAM.mif", "w+")
+
+        f.write("WIDTH = 32;")
+        f.write("\n")
+        f.write("DEPTH = 16383;")
         f.write("\n")
         f.write("\n")
-        
+
         f.write("ADDRESS_RADIX = UNS;")
         f.write("\n")
         f.write("DATA_RADIX = BIN;")
@@ -40,41 +38,37 @@ while(1):
         f.write("CONTENT BEGIN")
         f.write("\n")
 
-        i,j,k = 0,0,0
-        filtro = [0,0,0,0,1,0,0,0,0]
-        
-        #for x in filtro:
-            
-        #    f.write("\t")
-        #    f.write(str(k) + " : " + str( bin(x & 0xff)[2:].zfill(8) ) + ";")
-        #    f.write("\n")
-        #    k += 1
+        i, j, k = 0, 0, 0
 
         height, width = resize.shape[:2]
-        
-        while(i<height):
-            while(j<width):
-                px = resize[i,j]
+
+        while(i < height):
+            while(j < width):
+                px_1 = resize[i, j]
+                px_2 = resize[i + 1, j + 1]
+                px_3 = resize[i + 2, j + 2]
+                px_4 = resize[i + 3, j + 3]
+
+                px_1 = str(bin(px_1)[2:].zfill(8))
+                px_2 = str(bin(px_2)[2:].zfill(8))
+                px_3 = str(bin(px_3)[2:].zfill(8))
+                px_4 = str(bin(px_4)[2:].zfill(8))
+
+                pixels = px_1 + px_2 + px_3 + px_4
                 f.write("\t")
-                f.write(str(k) + " : " + str(bin(px)[2:].zfill(8)) + ";")
+                f.write(str(k) + " : " + pixels + ";")
                 f.write("\n")
-               # output[i,j] = int(bin(px)[2:].zfill(8),2) + 100
-                j += 1
+                j += 4
                 k += 1
-            i += 1
+            i += 4
             j = 0
 
-       
-
         f.write("\t")
-        f.write("[" + str(k) + ".." + str(63536 - 1) + "]" + " : " + str(bin(0)[2:].zfill(8)) + ";")
+        f.write("[" + str(k) + ".." + str(16383) + "]" +
+                " : " + str(bin(0)[2:].zfill(32)) + ";")
         f.write("\n")
         f.write("END;")
         f.close()
 
-        cv2.imwrite('output.jpg', output)
         print("Listo!")
-        print("------------------------------------------")
-    elif(int(opcion) == 2):
-        print("Cargando volcado de memoria")
         print("------------------------------------------")
